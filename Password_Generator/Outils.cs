4a78e42408c7475ee076e984b0b9e8003ccdb71f
Onur Ozogul <onurozogul1@gmail.com>
2021-08-17 20:04:16 +0200

    Ajoutez des fichiers projet.

---
+++ Password_Generator/Outils.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationCS
+{
+     static class Outils
+    {
+        public static int DemanderNombrePositifNonNul(string question)
+        {
+            //int positif = DemanderNombre(question);
+            //if (positif >0)
+            //{
+            //    return positif; 
+            //}
+            //Console.WriteLine("ERROR : Veuillez entrer un nombre positif");
+
+            //return DemanderNombrePositifNonNul(question); 
+            return DemanderNombreEntre(question, 1, int.MaxValue, "ERROR: Le nombre doit être positif et non nul ");
+        }
+        public static int DemanderNombreEntre(string question, int min, int max, string messageErreur = null)
+        {
+
+
+
+            int nombre = DemanderNombre(question);
+
+            if ((nombre <= max) && (nombre >= min))
+            {
+                return nombre;
+
+            }
+            if (messageErreur ==null)
+            {
+                Console.WriteLine("ERROR : Le nombre doit etre compris entre  " + min + " et " + max);
+            }
+            else
+            {
+                Console.WriteLine(messageErreur);
+            }
+               
+                Console.WriteLine();
+
+            
+            return DemanderNombreEntre(question, min, max); //C'est une fonction récursive, ca permet de boucler comme avec while
+
+
+        }
+        public static int DemanderNombre(string question)
+        {
+            while (true)
+            {
+                Console.WriteLine(question);
+                string reponse = Console.ReadLine();
+                try
+                {
+
+                    int reponseInt = int.Parse(reponse);
+                    return reponseInt;
+                }
+                catch
+                {
+
+                    Console.WriteLine("ERROR: Veuillez entrer un nombre");
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ Password_Generator/Program.cs
@@ -0,0 +1,74 @@
+﻿using FormationCS;
+using System;
+
+
+namespace Password_Generator
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            const int nb_mdp = 10; 
+           
+            int longueurMotDePasse = Outils.DemanderNombrePositifNonNul("Longueur du mot de passe : ");
+            Console.WriteLine();
+            int choixAlphabet = Outils.DemanderNombreEntre("Vous voulez un mot de passe avec :\n" + 
+                "1- Uniquement des caractères en miniscule\n" +
+                "2- Des caractères miniscules et majuscules\n" + 
+                "3- Des caractères et des chiffres\n" + 
+                "4- Caractères, chiffres et caractères spécieaux\n" +
+                "Votre choix : ", 1, 4);
+
+          
+            string miniscules = "abcdefghijklmnopqrstuvwxyz";
+            string majuscules =miniscules.ToUpper();
+            string chiffres = "0123456789";
+            string caracteresS = "€#&+-¤$ù%!?";
+            string alphabet;
+            string motDePasse = "";
+          
+            Random rand = new Random();
+
+            if (choixAlphabet ==1)
+            {
+                alphabet = miniscules;
+            }
+            else if (choixAlphabet==2)
+            {
+                alphabet = miniscules + majuscules; 
+            }
+            else if (choixAlphabet==3)
+            {
+                alphabet = miniscules + majuscules + chiffres; 
+            }
+            else
+            {
+                alphabet = miniscules + majuscules + chiffres + caracteresS; 
+            }
+            int l = alphabet.Length;
+            int num = 0;
+            for (int j = 0; j < nb_mdp; j++)
+            {
+                motDePasse = "";
+                num++;
+                for (int i = 0; i < longueurMotDePasse; i++)
+                {
+                    int index = rand.Next(0, l);
+                    motDePasse += alphabet[index];
+                    
+
+                }
+               
+                Console.WriteLine("Password " +num + " =" + motDePasse);
+            }
+
+            
+            
+
+
+
+
+        }
+    }
+}
